4e45d5c70cb7351e5842875226bcc5d54b17d0bb
Mohamed Miloudi <milou0104@gmail.com>
2018-03-26 12:24:38 +0100

    update to bootstrap 4

--- src/views/emails/resend.blade.php
+++ src/views/emails/resend.blade.php
@@ -3,33 +3,30 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Email confirmation resend</div>
+        <div class="col-md-8 offset-2">
+            <div class="card card">
+                <div class="card-header">Email confirmation resend</div>
 
-                <div class="panel-body">
+                <div class="card-body">
 
                     @include('Laraconfirm::emails.partials._alerts');
 
                     <form class="form-horizontal" method="POST" action="{{ route('email.confirmation.resend') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
+                        @csrf
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-4 col-form-label text-md-right">E-Mail Address</label>
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
+                                <input id="email" type="email" class="form-control {{ $errors->has('email') ? ' is-invalid' :  '' }}" name="email" value="{{ old('email') }}" required>
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
+                                    <div class="invalid-feedback">
                                         <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
+                                    </div>
                                 @endif
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-6 offset-4">
                                 <button type="submit" class="btn btn-primary">
                                     Resend email
                                 </button>
